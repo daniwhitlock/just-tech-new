ced7141e90773453ac84edd74b3566594ab6ccae
Fire Dragon <daniwhitlock4@gmail.com>
2021-02-14 07:09:29 -0700

    compared code to snapshot code, deleted some unnecessary comments

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,6 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-  // Access our User model and run .findAll() method
   User.findAll({
     attributes: { exclude: ['password'] }
   })
@@ -14,7 +13,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
@@ -27,7 +25,6 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
-      
       res.json(dbUserData);
     })
     .catch(err => {
@@ -36,7 +33,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// POST /api/users
 router.post('/', (req, res) => {
   // expects {usernmae: 'Lernantion', email: 'lernantino@gmail.com', passwword: 'password1234;}
   User.create({
@@ -64,6 +60,7 @@ router.post('/login', (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
@@ -73,7 +70,6 @@ router.post('/login', (req, res) => {
   });
 });
 
-// PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -97,7 +93,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
