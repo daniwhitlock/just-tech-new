852e437be5a9f8e4b9d4ea3f5e17d0df72cb1df2
Fire Dragon <daniwhitlock4@gmail.com>
2021-02-21 07:00:59 -0700

    added if statements to view logout if logged in on all pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,7 +32,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
       console.log(dbPostData);
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
