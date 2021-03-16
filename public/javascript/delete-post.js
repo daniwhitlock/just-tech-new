a94070b7f22e6dc4bea15a3a3b8194082acb1221
Fire Dragon <daniwhitlock4@gmail.com>
2021-03-15 20:02:13 -0600

    finished added delete and edit post

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,11 +1,11 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
 
-    const post_id = window.location.toString().split('/')[
+    const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-    
-    const response = await fetch('/api/posts/${id}', {
+
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE'
     });
 
